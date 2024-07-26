04a14b98dffb10c0fbd3349c8ef47e1c88c05586
Andrey Kuz <140019599+andreikuznetsov02201@users.noreply.github.com>
2024-07-26 20:11:30 +0300

    working

---
+++ file.js
@@ -0,0 +1,27 @@
+'use strict'
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"]
+
+console.log(arr[0]);
+
+
+//let answer = +prompt("Есть ли вам 18 лет?", "Да");
+//https://learn.javascript.ru/first-steps подсказки
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_precedence "es6"
+
+let incr = 10,
+    decr = 10;
+
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
