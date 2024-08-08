949c11c41958662f7fb5c0c34013734db64db675
Andrey Kuznetsov <andr.kuzn10@gmail.com>
2024-08-08 19:10:26 +0300

    change name

--- yoga/file.js
+++ yoga/file.js
@@ -1,33 +1,33 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() {    //когда загрузилась вся структура DOM(html) картинки и тд не обяз
 
     'use strict';
-    let tab = document.querySelectorAll('.info-header-tab'),
+    let tab = document.querySelectorAll('.info-header-tab'),    //передаём элементы
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
+    function hideTabContent(a) {    //скрывает лишние табы
         for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
+            tabContent[i].classList.remove('show');     //удаляем show     //будем использовать каждый tabContent
+            tabContent[i].classList.add('hide');        //доб класс чтобы спрятать --- смотри css
         }
     }
 
-    hideTabContent(1);
+    hideTabContent(1);  //это---a
 
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('show');
+    function showTabContent(b) {    //показывает tabContent
+        if (tabContent[b].classList.contains('hide')) {     //проверяем скрыт ли этот элемент
+            tabContent[b].classList.remove('hide');     //удаляем hide
+            tabContent[b].classList.add('show');    //доб show(показать)
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function(event) {    //назначение обработчика событий (панель сверху(лечение, отдых ...))
         let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
+        if (target && target.classList.contains('info-header-tab')) {   //проверям что мы кликнули
+            for(let i = 0; i < tab.length; i++) {   //перебираем ТАБЫ
+                if (target == tab[i]) {     //если то куды нажали совпадает с опред ТАБОМ то....
+                    hideTabContent(0);      //скроем все ненужные табы
+                    showTabContent(i);      //показывем только тот который совпадает с ТАБОМ
                     break;
                 }
             }
